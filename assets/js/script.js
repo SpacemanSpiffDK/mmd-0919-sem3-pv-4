c8729ed8bdbc40d8952dd7633762e6aace553af6
Dan Høegh <dan.hoegh@gmail.com>
2020-08-31 15:28:08 +0200

    cut code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,5 @@
 // UCN MMD 2020
 
 // Setting options in a json object
-const options = {
-    "controls": true, 
-    "autoplay": false, 
-    "preload": "auto", 
-    "muted": false
-};
 
 // activate the video
-videojs('video1', options);
